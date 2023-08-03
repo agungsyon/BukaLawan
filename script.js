df9a96f050b9651c22f911a88f5c24016226a6b4
dimas abimanyu <dimasmanyu99@gmail.com>
2023-08-04 00:13:20 +0700

    dimas

--- script.js
+++ script.js
@@ -104,58 +104,119 @@ let data = [
     img: "./data-img/ips-cerdas-cermat.jpg",
   },
 ];
-
-let keranjang = {};
+// cart
+let countainerCart = [];
 
 const el = document.getElementById("booklist");
+const bodypayment = document.getElementById("bodypayment");
 
 let cards = "";
+let paymets = "";
 
-for (const book of data) {
-  let { id, title, price, stock, description, img } = book;
+// jalan di awal
+loopBuku();
+function loopBuku() {
+  el.innerHTML = "";
+  cards = "";
+  for (const book of data) {
+    let { id, title, price, stock, description, img } = book;
 
-  const card = `
-    <div class="col mb-5 ">
-    <div class="card h-100 ">
-        <!-- Product image-->
-        <img class="card-img-top card-img-bukalawan" src="${img}" alt="..." width="350" height="350"/>
-        <!-- Product details-->
+    const card = `
+      <div class="col mb-5 ">
+      <div class="card h-100 ">
+          <!-- Product image-->
+          <img class="card-img-top card-img-bukalawan" src="${img}" alt="..." width="350" height="350"/>
+          <!-- Product details-->
+  
+          <div class="card-body p-4 ">
+              <div class="text-center">
+                  <!-- Product name-->
+                  <h5 class="fw-bolder">${title}</h5>
+                  <p>${description}</p>
+                  <!-- Product price-->
+                  IDR.<B>${price}</B>
+                  <br>
+                  Stock : ${stock}
+              </div>
+          </div>
+          <!-- Product actions-->
+          <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+              <div class="text-center"><a onclick="beli(${id})" class="btn btn-outline-dark mt-auto" href="#">Beli Buku</a></div>
+          </div>
+      </div>
+      </div>`;
+    cards += card;
+  }
 
-        <div class="card-body p-4 ">
-            <div class="text-center">
-                <!-- Product name-->
-                <h5 class="fw-bolder">${title}</h5>
-                <p>${description}</p>
-                <!-- Product price-->
-                IDR.<B>${price}</B>
-                <br>
-                Stock : ${stock}
-            </div>
-        </div>
-        <!-- Product actions-->
-        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-            <div class="text-center"><a onclick="beli(${id})" class="btn btn-outline-dark mt-auto" href="#">Beli Buku</a></div>
-        </div>
-    </div>
-    </div>`;
-  cards += card;
+  el.innerHTML = cards;
 }
-
-el.innerHTML = cards;
-
-let countainerCart = []
 
 function beli(id) {
   let counting = document.getElementById("counting");
   let cart = Number(counting.innerText);
-  
+
   cart++;
 
-  counting.innerText = cart.toString()
+  counting.innerText = cart.toString();
 
-  const product = data.find(x => x.id == id)
-  countainerCart.push(product)
-  console.log(countainerCart);
+  const product = data.find((x) => x.id == id);
+  countainerCart.push(product);
+  console.log(product);
+  product.stock--;
+  loopBuku();
+  addToModal();
 }
 
- 
+function addToModal() {
+  bodypayment.innerHTML = "";
+  paymets = "";
+  for (const item of countainerCart) {
+    let { id, title, price, stock, description, img } = item;
+    const payment = `<div class="card rounded-3 mb-4">
+                        <div class="card-body p-4">
+                          <div
+                            class="row d-flex justify-content-between align-items-center">
+                            <div class="col-md-2 col-lg-2 col-xl-2">
+                              <img
+                                src="${img}"
+                                class="img-fluid rounded-3"
+                                alt="${title}" />
+                            </div>
+                            <div class="col-md-3 col-lg-3 col-xl-3">
+                              <p class="lead fw-normal mb-2">${title}</p>
+                            </div>
+                            <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
+                              <button
+                                class="btn btn-link px-2"
+                                onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
+                                <i class="fas fa-minus"></i>
+                              </button>
+  
+                              <input
+                                id="form1"
+                                min="0"
+                                name="quantity"
+                                value="2"
+                                type="number"
+                                class="form-control form-control-sm" />
+  
+                              <button
+                                class="btn btn-link px-2"
+                                onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
+                                <i class="fas fa-plus"></i>
+                              </button>
+                            </div>
+                            <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+                              <h5 class="mb-0">$499.00</h5>
+                            </div>
+                            <div class="col-md-1 col-lg-1 col-xl-1 text-end">
+                              <a href="#!" class="text-danger"
+                                ><i class="fas fa-trash fa-lg"></i
+                              ></a>
+                            </div>
+                          </div>
+                        </div>`;
+    paymets += payment;
+  }
+  bodypayment.innerHTML = paymets;
+}
