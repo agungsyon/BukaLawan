1391000e4e1ab1ce229670614296d85435563347
Agung Syon <ahmatagung645@gmail.com>
2023-08-03 14:20:49 +0700

    style

---
+++ script.js
@@ -0,0 +1,14 @@
+let data = [{
+    presiden : {
+        price : 100_000,
+        deskription : "buku tentang presiden",
+        stock : 1
+    },
+    megawati : {
+        price : 150_000,
+        deskription : "all about megawati",
+        stock : 2
+    }
+}];
+
+let keranjang = {}
